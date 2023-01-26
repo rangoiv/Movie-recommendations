0712f8b987ec5496da277e72b1e0087ea8a59b1b
karlmile <carla.miletic00@gmail.com>
2023-01-26 16:59:52 +0100

    dodala fold i unfold

--- matrix_factorization_test.py
+++ matrix_factorization_test.py
@@ -2,6 +2,8 @@ import numpy
 
 from matrix_factorization import matrix_factorization
 
+import unfold 
+
 R = [
 
     [5, 3, 0, 1],
@@ -33,6 +35,34 @@ nP, nQ = matrix_factorization(R, P, Q, K)
 
 nR = numpy.dot(nP, nQ.T)
 
-print(nR)
+# print(nR)
 
 # %%
+
+m=3
+n=4
+k=2
+T = numpy.array([[[1,4,7,10], [2,5,8,11], [3,6,9,12]],
+                [[13,16,19,22], [14,17,20,23], [15,18,21,24]]])
+                # [[25,26,27,28], [29,30,31,32], [33,34,35,36]]])
+
+# T = numpy.array([[[1,13],[2,14],[3,15],[4,16]],[[5,17],[6,18],[7,19],[8,20]],[[9,21],[10,22],[11,23],[12,24]]])
+
+
+print(numpy.shape(T))
+print(T)
+M = unfold.unfold(T, 1)
+print("1:\n",M)
+T=unfold.fold(M, 1, (k,m,n))
+print(T)
+M = unfold.unfold(T, 2)
+print("2:\n", M)
+T=unfold.fold(M, 2, [k,m,n])
+print(T)
+M = unfold.unfold(T, 3)
+print("3:\n",M)
+T=unfold.fold(M, 3, [k,m,n])
+print(T)
+
+
+
