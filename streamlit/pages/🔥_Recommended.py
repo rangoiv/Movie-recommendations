9110c7ed334a3fecfd1730da274446e2682e3ce2
karlmile <carla.miletic00@gmail.com>
2023-02-19 16:01:21 +0100

    added recommended movies and fixed bugs in movie ratings

--- streamlit/pages/🔥_Recommended.py
+++ streamlit/pages/🔥_Recommended.py
@@ -1,12 +1,62 @@
 import streamlit as st
 import pandas as pd
+import sys
+import numpy as np
+sys.path.append('../')
+from tensor_factorization import initialize_factorization, D, fine_tune, Lambda
+from tensor_factorization import evaluate
+from FactorizationRatingsAprox import FactorizationRatingsAprox
+from split_data import split_data
+from sparse_array import NDSparseArray
+from datetime import datetime
+from PIL import Image
+import requests
 
 
 st.set_page_config(
     page_title="Recommended movies",
     page_icon="intelligence.jpg",
 )
+no_image = Image.open(".\\image_not_available.jpg")
 
-st.title("Movies ratings")
 
-st.write(st.session_state)
+st.title("TOP 10 RECOMMENDED MOVIES")
+
+my_ratings = st.session_state
+
+movies_ = pd.read_csv("movies_metadata.csv", low_memory=False)
+
+movies = movies_[['id', 'title', 'poster_path', 'imdb_id', 'popularity']]
+
+
+model_path = "../factorization_movies_model.pkl"
+
+obj = FactorizationRatingsAprox.from_file(model_path)
+
+ratings = []
+for key in my_ratings.keys():
+    if my_ratings[key]!=0.0 and int(key)<=163950:
+        list =[int(key), my_ratings[key]]
+        ratings.append(list)
+
+print(ratings)
+
+recommended = obj.evaluate(ratings)
+
+
+j=1
+for i in range (10):
+    movie = movies[movies['id'] == str(recommended[i][0])]
+    
+    try: 
+        st.header(str(j) + ". " + movie.iloc[0]['title'])
+        response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[0]['poster_path'])
+        if response.status_code == 200:
+            st.image(response.content)
+        else:
+            st.image(no_image)
+        st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[0]['imdb_id'] + ")")
+        j=j+1
+    except:
+        print("Nema filma")
+    
--- streamlit/📈_Rate_movies.py
+++ streamlit/📈_Rate_movies.py
@@ -17,8 +17,8 @@ st.set_page_config(
 
 st.title("Movies ratings")
 
-image = Image.open(".\image_not_available.jpg")
-data = pd.read_csv("movies_metadata.csv")
+no_image = Image.open(".\\image_not_available.jpg")
+data = pd.read_csv(".\\movies_metadata.csv", low_memory=False)
 columns = data.columns
 
 # st.write(columns)
@@ -26,7 +26,9 @@ columns = data.columns
 
 movie = data[['id', 'title', 'poster_path', 'imdb_id', 'popularity']]
 
-movie.drop(movie[movie['popularity'] == "Beware Of Frost Bites"].index, inplace = True)
+# movie[] = movie[movie['popularity'] == "Beware Of Frost Bites"]
+# movie['popularity'] = movie['popularity'].replace(['Beware Of Frost Bites'], '0.0')
+movie = movie[movie['popularity'] != 'Beware Of Frost Bites']
 
 movie['popularity'] = movie['popularity'].astype(float)
 
@@ -37,54 +39,73 @@ movie['popularity'] = movie['popularity'].astype(float)
 movie = movie.sort_values(by=['popularity'], ascending=False)
 
 movie = movie.head(100)
-movie = movie.sample(n=12)
+movie = movie.sample(n=20)
 
 
-
+i=0
+j=0
 with st.form("Form"):
     
     col1, col2, col3 = st.columns(3, gap="large")
     with col1:
-        for i in range (4):
-            st.header(movie.iloc[i]['title'])
-            response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[i]['poster_path'])
-            if response.status_code == 200:
-                st.image(response.content)
-            else:
-                st.image(image)
-            st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[i]['imdb_id'] + ")")
-            rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
+        while i<4:
+            try:
+                st.text(movie.iloc[j]['title'])
+                response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[j]['poster_path'])
+                if response.status_code == 200:
+                    st.image(response.content)
+                else:
+                    st.image(no_image)
+                st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[j]['imdb_id'] + ")")
+                rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
 
-            rating_list[movie.iloc[i]['id']] = rating
-            # st.write(st.session_state)
+                rating_list[movie.iloc[i]['id']] = rating
+                # st.write(st.session_state)
+                i=i+1
+                j=j+1
+            except:
+                print("Loš poster")
+                j=j+1
 
 
     with col2:
-        for i in range (4,8):
-            st.header(movie.iloc[i]['title'])
-            response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[i]['poster_path'])
-            if response.status_code == 200:
-                st.image(response.content)
-            else:
-                st.image(image)
-            st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[i]['imdb_id'] + ")")
-            rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
-            rating_list[movie.iloc[i]['id']]= rating
+        while i<8:
+            try:
+                st.text(movie.iloc[j]['title'])
+                response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[j]['poster_path'])
+                if response.status_code == 200:
+                    st.image(response.content)
+                else:
+                    st.image(no_image)
+                st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[j]['imdb_id'] + ")")
+                rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
+                rating_list[movie.iloc[i]['id']]= rating
+                i=i+1
+                j=j+1
+            except:
+                print("Loš poster")
+                j=j+1
 
 
     
 
     with col3:
-        for i in range (8, 12):
-            st.header(movie.iloc[i]['title'])
-            response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[i]['poster_path'])
-            if response.status_code == 200:
-                st.image(response.content)
-            else:
-                st.image(image)
-            st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[i]['imdb_id'] + ")")
-            rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
-            rating_list[movie.iloc[i]['id']]= rating
+        while i<12:
+            try:
+                st.text(movie.iloc[j]['title'])
+                response = requests.get("https://image.tmdb.org/t/p/original" + movie.iloc[j]['poster_path'])
+                if response.status_code == 200:
+                    st.image(response.content)
+                else:
+                    st.image(no_image)
+                st.write(" Watch [trailer](https://www.imdb.com/title/" + movie.iloc[j]['imdb_id'] + ")")
+                rating=st.slider("Ocijeni film: ", 0.0, 5.0, step = 0.5, key = i)
+                rating_list[movie.iloc[i]['id']]= rating
+                i=i+1
+                j=j+1
+            except:
+                print("Loš poster")
+                j=j+1
 
 
     submitted = st.form_submit_button("Submit")
